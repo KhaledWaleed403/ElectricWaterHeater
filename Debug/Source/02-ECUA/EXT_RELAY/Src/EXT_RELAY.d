c2913113197d9e8fe997f10e74d38c2bfd8b0a06
Khaled Waleed <45642478+KhaledWaleed403@users.noreply.github.com>
2024-12-09 23:48:52 +0200

    Add files via upload

---
+++ Debug/Source/02-ECUA/EXT_RELAY/Src/EXT_RELAY.d
@@ -0,0 +1,18 @@
+Source/02-ECUA/EXT_RELAY/Src/EXT_RELAY.o \
+  Source/02-ECUA/EXT_RELAY/Src/EXT_RELAY.o:  \
+ ../Source/02-ECUA/EXT_RELAY/Src/EXT_RELAY.c \
+  C:\Users\Khaled\workspace\Electric_Water_Heater\Source\02-ECUA\EXT_RELAY\Inc/EXT_RELAY.h \
+  C:\Users\Khaled\workspace\Electric_Water_Heater\Source\01-MCAL\Dio\Inc/Dio.h \
+  C:\Users\Khaled\workspace\Electric_Water_Heater\Source\00-Lib/Std_Types.h \
+  C:\Users\Khaled\workspace\Electric_Water_Heater\Source\01-MCAL\Dio\Inc/Dio_Types.h \
+  C:\Users\Khaled\workspace\Electric_Water_Heater\Source\01-MCAL\Dio\Inc/Dio_reg.h
+
+C:\Users\Khaled\workspace\Electric_Water_Heater\Source\02-ECUA\EXT_RELAY\Inc/EXT_RELAY.h:
+
+C:\Users\Khaled\workspace\Electric_Water_Heater\Source\01-MCAL\Dio\Inc/Dio.h:
+
+C:\Users\Khaled\workspace\Electric_Water_Heater\Source\00-Lib/Std_Types.h:
+
+C:\Users\Khaled\workspace\Electric_Water_Heater\Source\01-MCAL\Dio\Inc/Dio_Types.h:
+
+C:\Users\Khaled\workspace\Electric_Water_Heater\Source\01-MCAL\Dio\Inc/Dio_reg.h:
